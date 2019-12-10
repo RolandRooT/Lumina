6448f088098b5d7f22d317bce265200fa581cb0d
LCVClass <keisuke.sato@lasallecollegevancouver.com>
2019-12-10 02:43:10 -0800

    commit

---
+++ Assets/Scripts/DestroyTimer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class DestroyTimer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("Timer", 9);
+    }
+    private void Timer()
+    {
+        DestroyImmediate(gameObject, true);
+    }
+}
---
+++ Assets/Scripts/Fireworks.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+
+public class Fireworks : MonoBehaviour
+{
+    [SerializeField]
+    GameObject fireworks;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
